6b73a94cc17e68a3a719e99378663165a347a7f9
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-05-30 11:06:19 +0530

    Vaccination Schema

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -424,4 +424,14 @@ router.get("/parents/count", async (req, res) => {
   }
 });
 
+// Route to get the count of vaccinations
+router.get("/vaccinations/count", async (req, res) => {
+  try {
+      const count = await vaccinationSchema.countDocuments();
+      res.status(200).json({ count });
+  } catch (err) {
+      res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router; // Export the router instance
